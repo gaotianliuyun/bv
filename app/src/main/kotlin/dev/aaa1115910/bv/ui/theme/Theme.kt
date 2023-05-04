6153ffbb8d0bdfc691d74959e93c01f22b335ffc
gaotianliuyun <58679624+gaotianliuyun@users.noreply.github.com>
2023-05-04 08:04:49 +0800

    Add files via upload

--- app/src/main/kotlin/dev/aaa1115910/bv/ui/theme/Theme.kt
+++ app/src/main/kotlin/dev/aaa1115910/bv/ui/theme/Theme.kt
@@ -7,31 +7,26 @@ import androidx.compose.foundation.IndicationInstance
 import androidx.compose.foundation.LocalIndication
 import androidx.compose.foundation.interaction.InteractionSource
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Typography
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.ContentDrawScope
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalView
-import androidx.compose.ui.unit.Density
 import androidx.core.view.WindowCompat
-import androidx.tv.material3.ExperimentalTvMaterial3Api
-import androidx.tv.material3.MaterialTheme
-import androidx.tv.material3.Typography
-import androidx.tv.material3.darkColorScheme
+import dev.aaa1115910.bv.component.Disclaimer
 import dev.aaa1115910.bv.component.FpsMonitor
-import dev.aaa1115910.bv.component.SurfaceWithoutClickable
 import dev.aaa1115910.bv.util.Prefs
 
-@OptIn(ExperimentalTvMaterial3Api::class)
-private val bvColorSchemeTv = darkColorScheme(
+private val BiliColorScheme = darkColorScheme(
     primary = md_theme_dark_primary,
     onPrimary = md_theme_dark_onPrimary,
     primaryContainer = md_theme_dark_primaryContainer,
@@ -43,44 +38,20 @@ private val bvColorSchemeTv = darkColorScheme(
     tertiary = md_theme_dark_tertiary,
     onTertiary = md_theme_dark_onTertiary,
     tertiaryContainer = md_theme_dark_tertiaryContainer,
-    onTertiaryContainer = md_theme_dark_onTertiaryContainer,
-    background = Color(0xFF121212)
+    onTertiaryContainer = md_theme_dark_onTertiaryContainer
 )
 
-private val bvColorSchemeCommon = androidx.compose.material3.darkColorScheme(
-    primary = md_theme_dark_primary,
-    onPrimary = md_theme_dark_onPrimary,
-    primaryContainer = md_theme_dark_primaryContainer,
-    onPrimaryContainer = md_theme_dark_onPrimaryContainer,
-    //secondary = md_theme_dark_secondary,
-    //onSecondary = md_theme_dark_onSecondary,
-    //secondaryContainer = md_theme_dark_secondaryContainer,
-    //onSecondaryContainer = md_theme_dark_onSecondaryContainer,
-    tertiary = md_theme_dark_tertiary,
-    onTertiary = md_theme_dark_onTertiary,
-    tertiaryContainer = md_theme_dark_tertiaryContainer,
-    onTertiaryContainer = md_theme_dark_onTertiaryContainer,
-    background = Color(0xFF121212)
-)
-
-@OptIn(ExperimentalTvMaterial3Api::class)
 @Composable
 fun BVTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable (
-        onDensityChange: (Float) -> Unit
-    ) -> Unit
+    content: @Composable () -> Unit
 ) {
     val context = LocalContext.current
-    val fontScale = LocalDensity.current.fontScale
+    val colorScheme = BiliColorScheme
+    val typography =
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.M) android6AndBelowTypography else Typography()
+
     val view = LocalView.current
-
-    val colorScheme = bvColorSchemeTv
-    val typographyTv =
-        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.M) android6AndBelowTypographyTv else Typography()
-    val typographyCommon =
-        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.M) android6AndBelowTypographyCommon else androidx.compose.material3.Typography()
-
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
@@ -89,44 +60,25 @@ fun BVTheme(
         }
     }
 
-    val density = if (view.isInEditMode)
-        LocalDensity.current.density
-    else
-        Prefs.densityFlow.collectAsState(context.resources.displayMetrics.widthPixels / 960f).value
-
     val showFps by remember { mutableStateOf(if (!view.isInEditMode) Prefs.showFps else false) }
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = typographyTv
+        typography = typography
     ) {
-        androidx.compose.material3.MaterialTheme(
-            colorScheme = bvColorSchemeCommon,
-            typography = typographyCommon
+        CompositionLocalProvider(
+            LocalIndication provides NoRippleIndication,
         ) {
-            CompositionLocalProvider(
-                LocalIndication provides NoRippleIndication,
-                LocalDensity provides Density(density = density, fontScale = fontScale)
-            ) {
-                androidx.compose.material3.Surface(color = Color.Transparent) {
-                    SurfaceWithoutClickable {
-                        if (showFps) {
-                            FpsMonitor {
-                                content(
-                                    onDensityChange = {
-
-                                    }
-                                )
-                            }
-                        } else {
-                            content(
-                                onDensityChange = {
-
-                                }
-                            )
-                        }
+            Surface {
+                if (showFps) {
+                    FpsMonitor {
+                        content()
                     }
+                } else {
+                    content()
                 }
+
+                if (!view.isInEditMode) Disclaimer()
             }
         }
     }
